861bdf13c4c2b8826ea7320054d2fd03780d1997
damithBu <damithbuddika6561@gmail.com>
2022-11-09 12:19:27 +0530

    dami11/09 org 2

--- config/constansts.php
+++ config/constansts.php
@@ -8,7 +8,7 @@
                 define('DB_NAME','food-order');
 
              $conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD)or die (mysqli_error()); // database connection
-             $db_select = mysqli_select_db($conn,DB_NAME)or die (mysqli_error()); //SElecting database
+             $db_select = mysqli_select_db($conn,DB_NAME) or die (mysqli_error()); //SElecting database
 
 
 ?>
